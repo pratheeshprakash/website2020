1271a65a45c9e85dd3de9525fdb63a3bcdd3e218
Unknown <pratheeshprakash@gmail.com>
2020-05-19 11:08:38 +0530

    added js function for showing accordion menu
    open close animation works when used in .js function instead of add class

--- testing for scrolling video/script.js
+++ testing for scrolling video/script.js
@@ -21,18 +21,36 @@ const sceneA = new ScrollMagic.Scene({
     })
     .addIndicators() //used for only debugging and showing trigger handles
     .setPin(oneMoreSection) // used for locking the scene in position
-    // .setClassToggle("#collapseOne,#collapseTwo", "show") // add class toggle
     .addTo(controller);
-const sceneA1 = new ScrollMagic.Scene({
-        duration: 500, // length of video in ms
+
+//this scene is used to close all the accordion menus on exiting the main SceneA
+//this triggers a bit before the first sceneA1 and bit after sceneA3
+const sceneClose = new ScrollMagic.Scene({
+        duration: 1800, // length of video in ms
         triggerElement: oneMoreSection, //name of the element to use as trigger
-        offset: offSet+200, // this should be half the height of the video to work properly
+        offset: offSet+100, // this should be half the height of the video to work properly
         triggerHook: 0.5 // when to trigger 0 is top 0.5 mid and 1 is bottom
     })
     .addIndicators() //used for only debugging and showing trigger handles
-    // .setPin(oneMoreSection) // used for locking the scene in position
-    .setClassToggle("#collapseOne", "show") // add class toggle
+    .on("leave", function(event) {
+        $('#collapseOne').collapse('hide');
+        $('#collapseTwo').collapse('hide');
+        $('#collapseThree').collapse('hide');
+    })
     .addTo(controller);
+
+const sceneA1 = new ScrollMagic.Scene({
+        duration: 500, // length of video in ms
+        triggerElement: oneMoreSection, //name of the element to use as trigger
+        offset: offSet + 200, // this should be half the height of the video to work properly
+        triggerHook: 0.5 // when to trigger 0 is top 0.5 mid and 1 is bottom
+    })
+    .addIndicators() //used for only debugging and showing trigger handles
+    .on("enter", function(event) {
+        $('#collapseOne').collapse('show');
+    })
+    .addTo(controller);
+
 const sceneA2 = new ScrollMagic.Scene({
         duration: 500, // length of video in ms
         triggerElement: oneMoreSection, //name of the element to use as trigger
@@ -40,9 +58,11 @@ const sceneA2 = new ScrollMagic.Scene({
         triggerHook: 0.5 // when to trigger 0 is top 0.5 mid and 1 is bottom
     })
     .addIndicators() //used for only debugging and showing trigger handles
-    // .setPin(oneMoreSection) // used for locking the scene in position
-    .setClassToggle("#collapseTwo", "show") // add class toggle
+    .on("enter", function(event) {
+        $('#collapseTwo').collapse('show');
+    })
     .addTo(controller);
+
 const sceneA3 = new ScrollMagic.Scene({
         duration: 500, // length of video in ms
         triggerElement: oneMoreSection, //name of the element to use as trigger
@@ -50,9 +70,13 @@ const sceneA3 = new ScrollMagic.Scene({
         triggerHook: 0.5 // when to trigger 0 is top 0.5 mid and 1 is bottom
     })
     .addIndicators() //used for only debugging and showing trigger handles
-    // .setPin(oneMoreSection) // used for locking the scene in position
-    .setClassToggle("#collapseThree", "show") // add class toggle
+    .on("enter", function(event) {
+        $('#collapseThree').collapse('show');
+    })
     .addTo(controller);
+
+
+
 const scene = new ScrollMagic.Scene({
         duration: 5000, // length of video in ms
         triggerElement: scrollVideo, //name of the element to use as trigger
